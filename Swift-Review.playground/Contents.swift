0d6d6b52b01800cfc90eac6835b2b47c0759e20b
DitoShabri <sabrifirli@gmail.com>
2021-04-20 14:39:05 +0700

    Initial Commit

---
+++ Swift-Review.playground/Contents.swift
@@ -0,0 +1,110 @@
+import UIKit
+
+/*
+ 
+ Swift Review 1
+ 
+ String
+ 
+A. String
+
+var name : String
+name = "Dito"
+ 
+B. Integer
+ 
+var age : Int
+age = 22
+ 
+ Float
+ 
+
+var latitude : Float
+latitude = 36.66667
+
+var longtitude : Double
+longtitude = 36.12134555
+
+ Boolean
+ 
+var Benar = true
+Var Salah = false
+ 
+
+var  a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+
+b += 10
+b -= 10
+b *= b
+
+ Operator
+ 
+
+var a = 5.5
+var b = 2.2
+var c = a + b
+
+var name1 = "Dito"
+var name2 = "Sabri"
+var both = name1 + " and " + name2
+
+
+
+var d = 1.1
+var e = 2.2
+var f = d + e
+
+f  > 3
+f >= 3
+f > 4
+f < 4
+
+var name = "Dito"
+name == "dito"
+name != "dito"
+
+
+var IloveU = true
+IloveU
+!IloveU
+
+ 
+ String Interpolation
+ 
+
+
+var name = "Firli"
+var age = 22
+var latitude = 36.166667
+
+"Your name is \(name), yo're age \(age), your latitude is \(latitude)"
+
+"Your Name is " + name
+
+"Your Age is \(age), another age is \(age), years will be \(age * 2)"
+ 
+ 
+ Array
+
+
+var envenNumbers = [2, 4, 6, 8]
+var movie : [Any] = ["Men in black", "The Intern", "Palmer", 2015]
+
+type(of: movie)
+
+var songs = ["Shake it Off", "You Belong with Me", "Love Story"]
+var songs2 = ["Today was a Fairytale", "Welcome to New York", "Fifteen"]
+
+
+var both = songs + songs2
+
+both += ["Everthing His changed"]
+
+print(both)
+
+ */
